adb8a59e59972266b623b13ecca12a4d9b21b9a9
Maciej Petrykowski <m.petrykowski@gmail.com>
2016-07-10 22:35:39 +0200

    Schedule Builder added

---
+++ CQRS.Domain/Models/VoyageModel/ScheduleBuilder.cs
@@ -0,0 +1,45 @@
+﻿using CQRS.Domain.Models.LocationModel;
+using CQRS.Domain.Models.VoyageModel.Entities;
+using CQRS.Domain.Models.VoyageModel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Domain.Models.VoyageModel
+{
+    public class ScheduleBuilder
+    {
+        private readonly List<CarrierMovement> _carrierMovements = new List<CarrierMovement>();
+        private LocationId _departureLocation;
+
+        public ScheduleBuilder(
+            LocationId departureLocation)
+        {
+            _departureLocation = departureLocation;
+        }
+
+        public ScheduleBuilder Add(
+            LocationId arrivalLocationId,
+            DateTimeOffset departureTime,
+            DateTimeOffset arrivalTime)
+        {
+            _carrierMovements.Add(new CarrierMovement(
+                CarrierMovementId.New,
+                _departureLocation,
+                arrivalLocationId,
+                departureTime,
+                arrivalTime));
+
+            _departureLocation = arrivalLocationId;
+
+            return this;
+        }
+
+        public Schedule Build()
+        {
+            return new Schedule(_carrierMovements);
+        }
+    }
+}
